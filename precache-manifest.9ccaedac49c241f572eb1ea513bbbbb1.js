8cf29f077700a8de13dde8ba2d0b6afe19c6b7ce
VictoriaKaa <victoriaklimova96@gmail.com>
2020-05-19 00:54:39 +0300

    Updates

---
+++ precache-manifest.9ccaedac49c241f572eb1ea513bbbbb1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555c98ce2d389c0274e2d27fbbec57ac",
+    "url": "/journal/index.html"
+  },
+  {
+    "revision": "43d485a2b706e0b0e5e6",
+    "url": "/journal/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "92dafdeac48e7fcd2cb7",
+    "url": "/journal/static/css/main.4b688737.chunk.css"
+  },
+  {
+    "revision": "43d485a2b706e0b0e5e6",
+    "url": "/journal/static/js/2.38e7eaf6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/journal/static/js/2.38e7eaf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dafdeac48e7fcd2cb7",
+    "url": "/journal/static/js/main.36084026.chunk.js"
+  },
+  {
+    "revision": "a05f31f778b64f00f10b",
+    "url": "/journal/static/js/runtime-main.f1d0dc9a.js"
+  },
+  {
+    "revision": "8014a923629c214f45808c740e9de46d",
+    "url": "/journal/static/media/3725771.8014a923.jpg"
+  },
+  {
+    "revision": "b60c140a13d0477f71c9728bfb6638f4",
+    "url": "/journal/static/media/4f39.b60c140a.jpg"
+  },
+  {
+    "revision": "ff7ff7dab19bd6e72d836e2653d46cd9",
+    "url": "/journal/static/media/76901.ff7ff7da.jpg"
+  },
+  {
+    "revision": "692be682877dfe7afa550db272a0e6cd",
+    "url": "/journal/static/media/icomoon.692be682.svg"
+  },
+  {
+    "revision": "6c64b15f5c15c84ce639e04cee05e6db",
+    "url": "/journal/static/media/icomoon.6c64b15f.woff"
+  },
+  {
+    "revision": "a0b2a103e20f835d2e79bd79357ca705",
+    "url": "/journal/static/media/icomoon.a0b2a103.ttf"
+  },
+  {
+    "revision": "f30706e7753199c275b9fe396dcfe3d9",
+    "url": "/journal/static/media/icomoon.f30706e7.eot"
+  }
+]);
